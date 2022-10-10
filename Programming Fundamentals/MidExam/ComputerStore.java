e4504e0c1f44e17c9d17855ef5b2414029043c61
MLVelikova <magibox@yahoo.com>
2022-10-10 11:38:16 +0300

    creating new folder MidExam

---
+++ Programming Fundamentals/MidExam/ComputerStore.java
@@ -0,0 +1,36 @@
+package MidExam;
+
+import java.util.Scanner;
+
+public class ComputerStore {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputLine = scanner.nextLine();
+        double amount = 0;
+        while (!inputLine.equals("special") && !inputLine.equals("regular")) {
+            double price = Double.parseDouble(inputLine);
+            if (price < 0) {
+                System.out.println("Invalid price!");
+            } else {
+                amount += price;
+            }
+            inputLine = scanner.nextLine();
+        }
+        double taxes = amount * 0.2;
+        double amountWithTaxes = amount + taxes;
+        if (inputLine.equals("special")) {
+            amountWithTaxes = amountWithTaxes * 0.9;
+        }
+        if (amountWithTaxes == 0) {
+            System.out.println("Invalid order!");
+        } else {
+            System.out.println("Congratulations you've just bought a new computer!");
+            System.out.printf("Price without taxes: %.2f$%n", amount);
+            System.out.printf("Taxes: %.2f$%n", taxes);
+            System.out.println("-----------");
+            System.out.printf("Total price: %.2f$%n", amountWithTaxes);
+        }
+
+    }
+
+}
