7e0a94769d9da6dd1c334e63d4116f0fca9850c1
MLVelikova <magibox@yahoo.com>
2023-01-09 07:11:20 +0200

    add task

---
+++ Java Advanced/multidimensionalArraysExercise/ReverseMatrixDiagonals.java
@@ -0,0 +1,72 @@
+package multidimensionalArraysExercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseMatrixDiagonals {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split("\\s+");
+        int r = Integer.parseInt(input[0]);
+        int c = Integer.parseInt(input[1]);
+
+        int[][] matrix = new int[r][c];
+
+
+        fillMatrix(scanner, matrix, r, c);
+
+        printReverseDiagonal(matrix, r, c);
+
+
+    }
+
+    private static void printReverseDiagonal(int[][] matrix, int r, int c) {
+        int row = r - 1;
+        
+        for (int col = c - 1; col >= 0; col--) {
+
+            System.out.print(matrix[row][col] + " ");
+            int dr = row;
+            int dc = col;
+            while (dr - 1 >= 0 && dc + 1 <= c - 1) {
+                System.out.print(matrix[dr - 1][dc + 1] + " ");
+                dr--;
+                dc++;
+            }
+            System.out.println();
+
+
+        }
+        for (row = r - 1 - 1; row >= 0; row--) {
+
+            for (int col = 0; col < c; col++) {
+
+                System.out.print(matrix[row][col] + " ");
+                int dr = row;
+                int dc = col;
+                while (dr - 1 >= 0 && dc + 1 <= c - 1) {
+                    System.out.print(matrix[dr - 1][dc + 1] + " ");
+                    dr--;
+                    dc++;
+
+                }
+                System.out.println();
+                col = c;
+            }
+        }
+
+
+    }
+
+    private static void fillMatrix(Scanner scanner, int[][] matrix, int r, int c) {
+
+        for (int i = 0; i < r; i++) {
+            matrix[i] = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt).toArray();
+
+        }
+    }
+
+
+}
