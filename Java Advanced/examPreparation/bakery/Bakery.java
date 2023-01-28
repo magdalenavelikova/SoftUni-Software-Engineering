c8b95b1607f702d8df3075c8609b86ce8aa572f0
MLVelikova <magibox@yahoo.com>
2023-01-28 08:44:34 +0200

    bakery again

--- Java Advanced/examPreparation/bakery/Bakery.java
+++ Java Advanced/examPreparation/bakery/Bakery.java
@@ -5,46 +5,46 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Bakery {
-    private List<Employee> data;
+    private List<Employee> employees;
     private String name;
     private int capacity;
 
     public Bakery(String name, int capacity) {
-        data = new ArrayList<>();
+        employees = new ArrayList<>();
         this.name = name;
         this.capacity = capacity;
     }
 
     public void add(Employee employee) {
-        if (data.size() < capacity) {
-            data.add(employee);
+        if (employees.size() < capacity) {
+            employees.add(employee);
         }
     }
 
     public boolean remove(String name) {
-        return data.removeIf(e -> e.getName().equals(name));
+        return employees.removeIf(e -> e.getName().equals(name));
     }
 
     public Employee getOldestEmployee() {
-        return data.stream()
+        return employees.stream()
                 .max(Comparator.comparingInt(Employee::getAge))
                 .orElse(null);
 
     }
 
     public Employee getEmployee(String name) {
-        return data.stream().filter(e -> e.getName().equals(name)).findAny().orElse(null);
+        return employees.stream().filter(e -> e.getName().equals(name)).findAny().orElse(null);
     }
 
     public int getCount() {
-        return data.size();
+        return employees.size();
     }
 
     public String report() {
         StringBuilder output = new StringBuilder();
-        output.append(String.format("Employees working at Bakery %s:",this.name));
+        output.append(String.format("Employees working at Bakery %s:", this.name));
 
-        for (Employee employee : data) {
+        for (Employee employee : employees) {
             output.append(System.lineSeparator());
             output.append(employee.toString());
         }
