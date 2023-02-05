0662e70e5efea07192db0385777609493eb5e524
MLVelikova <magibox@yahoo.com>
2023-02-05 10:37:03 +0200

    еncapsulation

---
+++ OOP/еncapsulation/firstAndReserveTeam/Team.java
@@ -0,0 +1,42 @@
+package еncapsulation.firstAndReserveTeam;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Team {
+    private String name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public Team(String name) {
+        setName(name);
+        firstTeam = new ArrayList<>();
+        reserveTeam = new ArrayList<>();
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addPlayer(Person person) {
+        if (person.getAge() < 40) {
+            firstTeam.add(person);
+        } else {
+            reserveTeam.add(person);
+        }
+    }
+
+    public List<Person> getFirstTeam() {
+        return Collections.unmodifiableList(this.firstTeam);
+    }
+
+    public List<Person> getReserveTeam() {
+        return Collections.unmodifiableList(this.reserveTeam);
+    }
+
+}
---
+++ OOP/еncapsulation/salaryIncrease/Main.java
@@ -0,0 +1,28 @@
+package еncapsulation.salaryIncrease;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split(" ");
+            people.add(new Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3])));
+        }
+        double bonus = Double.parseDouble(reader.readLine());
+
+        for (Person person : people) {
+            person.increaseSalary(bonus);
+            System.out.println(person.toString());
+        }
+    }
+
+}
