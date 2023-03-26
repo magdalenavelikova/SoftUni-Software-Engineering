09c977ee175cea9dd40c71916b417624f525f656
MLVelikova <magibox@yahoo.com>
2023-03-26 17:56:50 +0300

    js update

--- JS Front-End/JS Syntax Fundamentals - Exercise/fruit.js
+++ JS Front-End/JS Syntax Fundamentals - Exercise/fruit.js
@@ -1,5 +1,5 @@
 function fruit(fruit, amount, price) {
-    console.log(`I need $${((amount * price) / 1000).toFixed(2)} to buy ${amount / 1000} kilograms ${fruit}.`)
+    console.log(`I need $${((amount * price) / 1000).toFixed(2)} to buy ${(amount / 1000).toFixed(2)} kilograms ${fruit}.`)
 }
 
 fruit('orange', 2500, 1.80);
