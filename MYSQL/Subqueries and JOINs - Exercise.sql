c8e6063c2d69b50cb6f1df865ceae559d5b0260d
MLVelikova <magibox@yahoo.com>
2023-05-02 16:19:24 +0300

    Exercise

--- MYSQL/Subqueries and JOINs - Exercise.sql
+++ MYSQL/Subqueries and JOINs - Exercise.sql
@@ -55,7 +55,57 @@ SELECT
     e.employee_id, e.first_name
 FROM
     employees AS e
-        JOIN
-    employees_projects AS ep ON e.employee_id <> ep.employee_id
-ORDER BY e.employee_id
+        LEFT JOIN
+    employees_projects AS ep ON e.employee_id = ep.employee_id
+WHERE
+    ep.project_id IS NULL
+ORDER BY e.employee_id DESC
 LIMIT 3;
+
+#6
+SELECT 
+    e.first_name, e.last_name,e.hire_date, d.name AS dept_name
+FROM
+    employees AS e
+    LEFT JOIN    departments AS d ON e.department_id = d.department_id
+WHERE
+    YEAR(e.hire_date) >1998 AND d.name IN('Sales','Finance')
+ORDER BY e.hire_date ;
+
+#7
+SELECT 
+    e.employee_id, e.first_name, p.name AS project_name
+FROM
+    employees AS e
+        JOIN
+    employees_projects AS ep ON ep.employee_id = e.employee_id
+        JOIN
+    projects AS p ON ep.project_id = p.project_id
+WHERE
+    DATE(p.start_date) > '2002-08-13'
+        AND p.end_date IS NULL
+ORDER BY e.first_name , p.name
+LIMIT 5;
+
+#8
+
+SELECT 
+    e.employee_id,
+    e.first_name,
+    (CASE
+        WHEN YEAR(p.start_date) > 2004 THEN 'NULL'
+        ELSE p.name
+    END) AS `project_name`,
+    p.start_date
+FROM
+    employees AS e
+        JOIN
+    employees_projects AS ep ON ep.employee_id = e.employee_id
+        JOIN
+    projects AS p ON ep.project_id = p.project_id
+WHERE
+    e.employee_id = 24
+ORDER BY p.name;
+
+
+
